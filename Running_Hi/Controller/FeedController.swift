69fdd835c9209748138bfe90b804c6a6eef3d4ff
Benny <wonsang222@naver.com>
2022-12-08 20:21:24 +0900

    시작

---
+++ Running_Hi/Controller/FeedController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedController.swift
+//  Running_Hi
+//
+//  Created by 황원상 on 2022/12/08.
+//
+
+import UIKit
+
+class FeedController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
