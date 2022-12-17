ca677a0c39b7d500713f74ce451357a05468528d
JongPyoAhn <whdvy95@gmail.com>
2022-12-17 23:08:51 +0900

    로그인 뷰 데모 완성 및 카카오 로그인 구현 중

---
+++ Running_Hi/AuthenticationScene/AuthenticationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationViewModel.swift
+//  Running_Hi
+//
+//  Created by 안종표 on 2022/12/17.
+//
+
+import Foundation
---
+++ Running_Hi/Service/Networks/KakaoNetworkService.swift
@@ -0,0 +1,12 @@
+//
+//  KakaoNetworkService.swift
+//  Running_Hi
+//
+//  Created by 안종표 on 2022/12/17.
+//
+
+import UIKit
+
+class KakaoNetworkService: NSObject {
+
+}
